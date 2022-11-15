ae1d0c89ab904549996732345229840b3d36c5ed
Richard Albert Leddy <richardaleddy@gmail.com>
2022-11-15 15:17:06 -0800

    Update Clock.svelte
    timezone selection

--- src/Clock.svelte
+++ src/Clock.svelte
@@ -1,14 +1,34 @@
 <script>
 	import { onMount } from 'svelte';
 
+	export let timezone
+
 	let time = new Date();
 
-	export let height = 100
-	export let width = 100
+	let night_or_day = '#000'
 
 	// these automatically update when `time`
 	// changes, because of the `$:` prefix
-	$: hours = time.getHours();
+	let tz_str = ""
+	let hr_update = 0
+	$: {
+		tz_str = time.toLocaleTimeString("en-US", { timeZone: timezone })
+		let tparts = tz_str.split(':')
+		hr_update = parseInt(tparts[0])
+
+		let rest = tparts[2].split(' ')
+		let night_n_day = rest[1].trim()
+
+		if ( night_n_day === "PM" ) {
+			night_or_day = '#000'
+		} else {
+			night_or_day = '#E96'
+		}
+
+	}
+	
+
+	$: hours = hr_update // time.getHours();
 	$: minutes = time.getMinutes();
 	$: seconds = time.getSeconds();
 
@@ -23,8 +43,8 @@
 	});
 </script>
 
-<svg width="{width}" height="{height}" viewBox='-50 -50 100 100'>
-	<circle class='clock-face' r='48'/>
+<svg  viewBox='-50 -50 100 100'>
+	<circle class='clock-face' r='48'  stroke="{night_or_day}"/>
 
 	<!-- markers -->
 	{#each [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55] as minute}
@@ -66,6 +86,7 @@
 		<line class='second' y1='10' y2='-38'/>
 		<line class='second-counterweight' y1='10' y2='2'/>
 	</g>
+
 </svg>
 
 <style>
@@ -75,26 +96,28 @@
 	}
 
 	.clock-face {
-		stroke: #333;
 		fill: white;
+		stroke-width: 3;
 	}
 
 	.minor {
 		stroke: #999;
-		stroke-width: 0.5;
+		stroke-width: 1.5;
 	}
 
 	.major {
 		stroke: #333;
-		stroke-width: 1;
+		stroke-width: 2;
 	}
 
 	.hour {
 		stroke: #333;
+		stroke-width: 4;
 	}
 
 	.minute {
 		stroke: #666;
+		stroke-width: 2;
 	}
 
 	.second, .second-counterweight {
@@ -102,6 +125,6 @@
 	}
 
 	.second-counterweight {
-		stroke-width: 3;
+		stroke-width: 6;
 	}
 </style>
